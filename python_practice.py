449c9d5178e22c78fb90e58cc9b39931c322aeec
pkassing <philipp.kassing@rl-institut.de>
2017-10-26 13:17:22 +0200

    now the generator is working

--- python_practice.py
+++ python_practice.py
@@ -2,26 +2,26 @@ import random
 
 def generator():
     password = ""
-    strength = int(input("How strong do you want your password to be? (weak, medium, strong)")) 
+    strength = str(input("How strong do you want your password to be? (weak, strong, very_strong)")) 
     elements= [["apple", "orange", "house", "blues", "horse", "shoe", "integer", "chair", "thermo", "beer"], ["1","2","3","4","5","6","7","8","9","10"],["a","d","g","e","w","q","t","z","h","v"],                  ["!","*", "§", "$", "%", "&", "/","3",">","5"]]
     
     
-    if strength == 1:
+    if strength == "weak":
         number = random.randint(0,9)
-        password += elements[0,number]
+        password += elements[0][number]
         print(password)
     
-    elif strength == 2:
+    elif strength == "strong":
         mylist= []
         for i in range(4):
             
             number1 = random.randint(0,3)
-            mylist.append(numbers[number1])
+            mylist.append(elements[number1][number1*2])
             number2 = random.randint(0,9)
-            mylist.append(elements[number1, number2])
+            mylist.append(elements[number1][number2])
             
             number3 = random.randint(0,9)
-            mylist.append(elements[number1, number3])
+            mylist.append(elements[number1][number3])
         
         
         
@@ -29,6 +29,25 @@ def generator():
         
         password +="".join(mylist)
         print(password) 
+    
+    elif strength == "very_strong":
+        mylist= []
+        for i in range(4):
+            
+            number1 = random.randint(0,3)
+            mylist.append(elements[number1][number1*2])
+            number2 = random.randint(0,9)
+            mylist.append(elements[number1][number2])
+            
+            number3 = random.randint(0,9)
+            mylist.append(elements[number1][number3])
+        
+        
+        
+        
+        
+        password +="".join(mylist)
+        print("Your password is :", password)
         
         
     #elif strength == "hard":
